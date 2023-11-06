c667fea69804e6d4d25f8ad1be813f124a9c39c3
John Zhou <chowjohn10@gmail.com>
2023-11-05 21:08:13 -0500

    Called an external API to do some cool stuff
    The REST API "Jikan" was used to fetch a random anime entry from myanimelist.net.
    
    Changes made:
    - Added a component called JikanComponent which contains a UI frame and a button that chooses a random anime when clicked on.
    - Replaced App.css with styles.css. styles.css contains new CSS styling
    elements that JikanComponent uses.
    - Set up a barebones structure in App.js for the App component, including a head and a body div.
    - A README writeup for this API will be done in a later pull request.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import JikanComponent from "./JikanComponent";
+import './styles.css'
+
 export default function App() {
   return (
     <div className="App">
-      Testing
+      <div className="header"><h1>JJEF having fun with API calls</h1></div>
+      <div className="body">
+		<JikanComponent />
+      </div>
     </div>
   );
 }
---
+++ src/JikanComponent.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+export default function JikanComponent() {
+  const [anime, setAnime] = useState({ data: {} });
+  const endpoint = "https://api.jikan.moe/v4/random/anime";
+
+  function handleGetRandomAnime() {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((responseJson) => setAnime(responseJson))
+      .catch((error) => console.error(error));
+  }
+
+  return (
+    <>
+      <div className="card">
+        <>
+          <MALResult anime={anime} />
+        </>
+      </div>
+      <button className="card-large-button" onClick={handleGetRandomAnime}>
+        <strong>Click for a random anime!</strong>
+      </button>
+    </>
+  );
+}
+
+function MALResult({ anime }) {
+  return (
+    <>
+      <div className="card-title">
+        <strong>{anime.data?.title}</strong>
+      </div>
+
+      {anime.data == null ? (
+        <h4>An unexpected error has occured. Please click the button again.</h4>
+      ) : (
+        // render image
+        <a href={anime.data?.url} target="_blank" rel="noopener noreferrer">
+          {isInappropriate(anime) ? (
+            <h4>
+              Image removed because it is likely NSFW (Click at your own risk!).
+            </h4>
+          ) : (
+            <img
+              src={anime.data?.images?.jpg?.image_url}
+              alt={anime.data?.images?.jpg?.image_url}
+              height={300}
+            ></img>
+          )}
+        </a>
+      )}
+
+      <div className="card-description">{anime.data?.synopsis}</div>
+    </>
+  );
+}
+
+function isInappropriate(anime) {
+  return (
+    anime.data?.rating?.includes("Rx") ||
+    anime.data?.rating?.includes("Hentai") ||
+    anime.data?.rating?.includes("R+")
+  );
+}
