a35fe16c98dcf9408d868a6ca9c6bfde251b04cc
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-11-08 15:55:08 -0500

    replaced price query with type selection instead

--- src/GetThingsToDo.js
+++ src/GetThingsToDo.js
@@ -11,32 +11,42 @@ function GetThingsToDo() {
     accessibility: null
   });
 
-  const [priceInput, setPriceInput] = useState(0);
+  const [typeInput, setTypeInput] = useState(0);
   const [activity, setActivity] = useState(null);
-  const [type, setType] = useState(null);
 
-  const endpoint = "https://www.boredapi.com/api/activity/?price=";
+  const endpoint = "https://www.boredapi.com/api/activity?type=";
 
   useEffect(() => {
     setActivity(boredJSONData.activity);
   }, [boredJSONData])
 
   function handleGetActivity() {
-    fetch(endpoint + priceInput)
+    fetch(endpoint + typeInput)
       .then(response => response.json())
         .then(jsonData => setboredJSONData(jsonData))
           .catch(error => console.error(error));
   }
 
-  function changePrice(priceText) {
-    setPriceInput(priceText.target.value);
+  function changeSelection(selection) {
+    setTypeInput(selection.target.value);
   }
 
   return (
     <>
       {activity && <p>{activity}</p>}
-      <input onChange={changePrice} value={priceInput}></input>
-      <button onClick={handleGetActivity}>Get an activity</button>
+      <select onChange={changeSelection} value={typeInput}>
+          <option value="">random</option> 
+          <option value="education">learning</option> 
+          <option value="recreational">for fun</option> 
+          <option value="social">with people</option> 
+          <option value="diy">do it yourself</option> 
+          <option value="charity">charity</option> 
+          <option value="cooking">cooking</option> 
+          <option value="relaxation">relaxing time</option> 
+          <option value="music">music</option> 
+          <option value="busywork">if you're really bored</option> 
+      </select>
+      <button onClick={() => {handleGetActivity()}}>Get an activity</button>
     </>
   )
 }
