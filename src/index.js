e9247d4b97985923d1b37e98287ced6e4bcf0263
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-11-21 17:28:45 -0500

    Added redux to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import store from './app/store';
+import { Provider } from 'react-redux';
+
 /* Routes */
 import App from './routes/App';
 import ErrorPage from './routes/ErrorPage';
@@ -44,8 +47,8 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Provider>
 );
 
