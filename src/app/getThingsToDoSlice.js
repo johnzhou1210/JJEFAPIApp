51207154f73e3f86b3ea181d777bb74507fcb619
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-11-21 22:35:03 -0500

    modified getThingsToDoSlice to include phrases arr

--- src/app/getThingsToDoSlice.js
+++ src/app/getThingsToDoSlice.js
@@ -3,15 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 export const boredPhraseSlice = createSlice({
     name: "boredPhrase",
     initialState: {
-        phrase: "Activity will appear here."
+        phrase: "Activity will appear here.",
+        phrases: [],
     },
     reducers: {
         showNextPhrase: (state, action) => {
             state.phrase = action.payload.activity;
+            console.log(state.phrase);
+        },
+        showPreviousPhrases: (state, action) => {
+            if (state.phrase !== "Activity will appear here.") {
+                state.phrases.push(action.payload);
+            }
         },
     }
 });
 
-export const { showNextPhrase } = boredPhraseSlice.actions;
+export const { showNextPhrase, showPreviousPhrases } = boredPhraseSlice.actions;
 
 export default boredPhraseSlice.reducer;
