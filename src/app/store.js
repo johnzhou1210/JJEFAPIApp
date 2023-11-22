39053f9e2df8cde8c0d0b8c55eb49d66a49f23f2
farhanaa-noor <80719003+farhanaa-noor@users.noreply.github.com>
2023-11-21 22:53:05 -0500

    Update store.js with catImageReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import catImageReducer from "./catImageSlice.js";
 export default configureStore({
   reducer: {
+    catImage: catImageReducer,
   },
 });
